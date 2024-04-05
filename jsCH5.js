6266a4f24c5d00defacde004590881d34bbe3fcd
dinaamlll <dinamaliaa17@gmail.com>
2024-04-05 22:50:48 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,10 @@
+video.addEventListener("click", function video(){
+    alert("Ini Video Taylor Swift");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("Ini Audio Taylor Swift")
+});
+gambar.addEventListener("click", function gambar(){
+    alert("Ini Gambar Taylor Swift ")
+});
